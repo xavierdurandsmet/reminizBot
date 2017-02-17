c5c7d3eb257be43b9e8b6519c70a8b4765c25d32
Antonin Archer <antoarcher@gmail.com>
2017-02-17 11:49:44 +0000

    Add Default Message Default message was missing. - add default message with gallery Also, add eslint configuration for style

--- index.js
+++ index.js
@@ -3,10 +3,9 @@ require('dotenv').config(({silent: true}))
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
-const Bing = require('node-bing-api')({ accKey: "00c98764dd9d440ba8d15bf161787d0e" })
-const wikipedia = require("node-wikipedia")
 const app = express()
 const mongoose = require('mongoose') // MongoDB lib
+const token = process.env.PAGE_ACCESS_TOKEN
 
 app.set('port', (process.env.PORT || 5000))
 
@@ -58,77 +57,61 @@ app.post('/webhook/', function (req, res) {
         let sender = event.sender.id
         if (event.message && event.message.text) {
             let userText = event.message.text
-            sendTextMessage(sender, userText)
+            sendDefaultMessage(sender, userText)
         }
     }
     res.sendStatus(200)
 })
 
-const token = process.env.PAGE_ACCESS_TOKEN
-
 // Send a text reply to a user
-function sendTextMessage(sender, text) {
-
-    Bing.images(text, {
-        top: 15,   // Number of results (max 50)
-        skip: 3    // Skip first 3 result
-    }, function (error, res, body) {
-
-        wikipedia.page.data(text, { content: true }, function(response) {
-	    // structured information on the page for Clifford Brown (wikilinks, references, categories, etc.)
-        console.log(response);
-        });
-
-        let messageData = {
-            "attachment": {
-                "type": "template",
-                "payload": {
-                    "template_type": "generic",
-                    "elements": [
+function sendDefaultMessage(sender) {
+  let messageData = {
+      "attachment": {
+          "type": "template",
+          "payload": {
+              "template_type": "generic",
+              "elements": [
+                  {
+                      "title": "CNN",
+                      "image_url": 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQn4O8zXpRf9xbk8vy0LdrXqa8jXUduoKdlc2YfrsL5cKxLBegR_e89HXg',
+                      "subtitle": "The news channel",
+                      "buttons": [
                         {
-                            "title": "Classic T-Shirt Collection",
-                            "image_url": body.value[0].contentUrl,
-                            "subtitle": "See all our colors",
-                            "default_action": {
-                                "type": "web_url",
-                                "url": "https://c65e40c2.ngrok.io",
-                                "messenger_extensions": true,
-                                "webview_height_ratio": "tall",
-                                "fallback_url": "https://c65e40c2.ngrok.io"
-                            },
-                            "buttons": [
-                                {
-                                    "title": "View",
-                                    "type": "web_url",
-                                    "url": "https://c65e40c2.ngrok.io",
-                                    "messenger_extensions": true,
-                                    "webview_height_ratio": "tall",
-                                    "fallback_url": "https://c65e40c2.ngrok.io"
-                                }
-                            ]
+                          "type":"postback",
+                          "title":"Choose ✔︎",
+                          "payload":"SINGLE_ACTOR"
                         }
-                    ]
-                }
-            }
-        }
-
-
-        request({
-            url: 'https://graph.facebook.com/v2.6/me/messages',
-            qs: { access_token: token },
-            method: 'POST',
-            json: {
-                recipient: { id: sender },
-                message: messageData,
-            }
-        }, function (error, response, body) {
-            if (error) {
-                console.log('Error sending messages: ', error)
-            } else if (response.body.error) {
-                console.log('Error: ', response.body.error)
-            }
-        })
-    });
+                      ]
+                  },
+                  {
+                      "title": "Disney Channel",
+                      "image_url": 'http://vignette4.wikia.nocookie.net/logopedia/images/8/87/Disney_Channel_2014.png/revision/latest?cb=20140522224840',
+                      "subtitle": "Children love it",
+                      "buttons": [
+                        {
+                          "type":"postback",
+                          "title":"Choose ✔︎",
+                          "payload":"MANY_ACTORS"
+                        }
+                      ]
+                  }
+              ]
+          }
+      }
+  }
+  request({
+      url: 'https://graph.facebook.com/v2.6/me/messages',
+      qs: { access_token: token },
+      method: 'POST',
+      json: {
+          recipient: { id: sender },
+          message: messageData,
+      }
+  }, function (error, response) {
+      if (error) {
+          console.log('Error sending messages: ', error)
+      } else if (response.body.error) {
+          console.log('Error: ', response.body.error)
+      }
+  })
 }
-
-
